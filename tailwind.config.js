5417e80b24a554be6e866cda35f215bbcebdf3a0
Hridoy <iamnirob334@gmail.com>
2024-02-10 00:31:12 +0600

    add course section

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,11 @@ module.exports = {
         transparant: "transparent",
         white: {
           100: "#fff",
+          500: "#F5F5F5",
+          900: "#D4D4D4",
         },
         primary: {
+          200: "#8EFFF3",
           500: "#00D0C8",
         },
         secondary: {
@@ -25,16 +28,27 @@ module.exports = {
         black: {
           900: "#171717",
           500: "#404040",
+          100: "#525252",
+        },
+        dark: {
+          800: "#1F2937",
+          600: "#4B5563",
+          900: "#111827",
+          950: "#030712",
         },
       },
       backgroundImage: {
         "gradient-100":
           "linear-gradient(180deg, #FFF 0%, rgba(255, 255, 255, 0.80) 0.01%, rgba(255, 255, 255, 0.20) 100%)",
         "gradient-900":
-          "linear-gradient(180deg, #000 0%, rgba(255, 255, 255, 0.80) 0.01%, rgba(0, 0, 0, 0.20) 100%)",
+          "linear-gradient(180deg, #000 0%, rgba(0, 0, 0, 0.80) 0.01%, rgba(0, 0, 0, 0.20) 100%)",
         // "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         // "gradient-conic":
         //   "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+
+      boxShadow: {
+        grey: "0px 2px 40px 0px rgba(14, 14, 14, 0.10)",
       },
     },
   },
