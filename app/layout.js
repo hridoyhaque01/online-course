c2df69d2254412aef89003919de2f4862b436f7c
Hridoy <iamnirob334@gmail.com>
2024-02-11 00:10:17 +0600

    add favicon

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Online Course",
+  title: "COURSEBES",
   description: "Online Course Tutorial",
 };
 
